d571ae413d514ebaddfbd7fdf29ffc04d3c75eca
KARTIK KATIYAR <82947445+katiyarkartik0@users.noreply.github.com>
2025-07-10 22:18:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const productRoutes = require('./routes/productRoutes');
 const app = express();
 app.use(express.json());
 
-// Product routes 
-app.use('/api/products', productRoutes);
-
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
